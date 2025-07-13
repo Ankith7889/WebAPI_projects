3587e0344c8486ebaa2bbf68939ddfdc47d85f36
Ankith7889 <ankithkullal7889@gmail.com>
2025-07-13 23:41:45 +0530

    Added first Web API project

---
+++ CollegeApp/Controllers/StudentController.cs
@@ -0,0 +1,24 @@
+﻿using CollegeApp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CollegeApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Student> GetStudentDetails()
+        {
+            return new List<Student> { new Student
+            {
+                Id = 1,
+                Name = "Ankith",
+                Email = "Ankith@gmail.com",
+                Phone = "8976573601"
+            }
+            };
+        }
+    }
+}

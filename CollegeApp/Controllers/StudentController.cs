f9afc8f3c23d295b7608a249b5287406c4844e08
Ankith7889 <ankithkullal7889@gmail.com>
2025-07-15 23:20:43 +0530

    Added Dtos

--- CollegeApp/Controllers/StudentController.cs
+++ CollegeApp/Controllers/StudentController.cs
@@ -9,30 +9,54 @@ namespace CollegeApp.Controllers
     public class StudentController : ControllerBase
     {
         [HttpGet]
-        public IEnumerable<Student> GetStudentDetails()
+        public IEnumerable<StudentDto> GetStudentDetails()
         {
-            return CollegeRepository.Students;
+            var students = CollegeRepository.Students.Select(s => new StudentDto
+            {
+                Id = s.Id,
+                Name = s.Name,
+                Email = s.Email,
+                Phone = s.Phone
+            });
+            return students;
         }
         [HttpGet("{id:int}")]
-        public ActionResult<Student> GetStudentById(int id)
+        [ProducesResponseType(200)]//documentaion of response types
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        public ActionResult<StudentDto> GetStudentById(int id)
         {   
             if (id <= 0)
                 return BadRequest();
             var student= CollegeRepository.Students.Where(s => s.Id == id).FirstOrDefault();
             if (student == null)
                 return NotFound();
-            return Ok(student);
+            var studentDto = new StudentDto
+            {
+                Id = student.Id,
+                Name = student.Name,
+                Email = student.Email,
+                Phone = student.Phone
+            };
+            return Ok(studentDto);
         }
 
         [HttpGet("{name:alpha}")]
-        public ActionResult<Student> GetStudentByname(string name)
+        public ActionResult<StudentDto> GetStudentByname(string name)
         {
             if (string.IsNullOrEmpty(name))
                 return BadRequest();
             var student = CollegeRepository.Students.Where(s => s.Name == name).FirstOrDefault();
             if (student == null)
                 return NotFound();
-            return Ok(student);
+            var studentDto = new StudentDto
+            {
+                Id = student.Id,
+                Name = student.Name,
+                Email = student.Email,
+                Phone = student.Phone
+            };
+            return Ok(studentDto);
         }
         [HttpDelete("{id:int}")]
         public bool DeleteStudentById(int id)

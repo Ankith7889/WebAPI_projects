31a3c40cdb47373088f0f06ef307885baf2af8a9
Ankith7889 <ankithkullal7889@gmail.com>
2025-07-20 22:42:44 +0530

    Added interface

---
+++ JobBoardApi/Interfaces/Interface.cs
@@ -0,0 +1,10 @@
+﻿using JobBoardApi.Models;
+
+public interface IJobRepository
+{
+    Task<IEnumerable<Job>> GetAllAsync();
+    Task<Job?> GetByIdAsync(int id);
+    Task AddAsync(Job job);
+    Task DeleteAsync(Job job);
+    Task SaveChangesAsync();
+}

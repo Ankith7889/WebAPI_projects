9551c8edbd624443ad0e0d008acd0a6e2e323023
Ankith7889 <ankithkullal7889@gmail.com>
2025-07-20 22:29:39 +0530

    added dbcontext

---
+++ JobBoardApi/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using JobBoardApi.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+
+namespace JobBoardApi.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {}
+        public DbSet<User> Users => Set<User>();
+        public DbSet<Job> Jobs => Set<Job>();
+        public DbSet<JobApplication> Applications => Set<JobApplication>();
+    }
+}

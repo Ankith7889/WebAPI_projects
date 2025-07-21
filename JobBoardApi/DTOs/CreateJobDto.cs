347ecdfc3c238165d1c0a66cce147afd03eabb33
Ankith7889 <ankithkullal7889@gmail.com>
2025-07-21 10:43:22 +0530

    added dtos

---
+++ JobBoardApi/DTOs/CreateJobDto.cs
@@ -0,0 +1,9 @@
+﻿namespace JobBoardApi.DTOs
+{
+    public class CreateJobDto
+    {
+        public string Title { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public string Company { get; set; } = null!;
+    }
+}
---
+++ JobBoardApi/DTOs/JobDto.cs
@@ -0,0 +1,10 @@
+﻿namespace JobBoardApi.DTOs
+{
+    public class JobDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = null!;
+        public string Company { get; set; } = null!;
+        public bool IsApproved { get; set; }
+    }
+}
